9458898bf95eea97219328fe943a13b0d8aadcd6
Unknown <steven.jabbour@gmail.com>
2019-04-04 14:57:52 +0200

    Fix tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../_layouts/App';
 
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+const hist = createBrowserHistory();
+
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render( < App / > , div);
+    ReactDOM.render( <Router history={hist}>
+        <Switch>
+          <Route path="/App" component={() => <App logoText="Pictet Technologies" />}  />
+          <Redirect from="/" to="/App" />
+        </Switch>
+      </Router>, div);
     ReactDOM.unmountComponentAtNode(div);
 });
